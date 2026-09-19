469fec8773e94e9c6c52b205f4a0e607c676490d
Ebrahim <ebrahim@gnu.org>
2026-09-20 02:24:30 +0330

    Minor

--- xmake.lua
+++ xmake.lua
@@ -2,10 +2,11 @@ set_allowedplats("windows")
 
 add_requires("hidapi")
 
-target("ds2xi")
+target("ds2xi", function()
     set_languages("c++20")
     set_kind("binary")
     add_includedirs("hidapi/hidapi", "ViGEmClient/include")
     add_files("main.cpp", "hidapi/windows/hid.c", "ViGEmClient/src/*.cpp")
     add_syslinks("user32", "kernel32", "shell32", "setupapi")
     set_runtimes("MT")
+end)
