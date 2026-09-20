a1da4724430f59c16c94e7fe90597280b2079c48
Ebrahim <ebrahim@gnu.org>
2026-09-20 21:56:48 +0330

    Minor comment tweak

--- main.cpp
+++ main.cpp
@@ -75,7 +75,8 @@ class Bridge
 		buffer[47 + isBluetooth] = (this->color >> 16) & 0xFF; // Blue component
 
 		if (this->ledNumber == 0)
-			// 0b00000, 0b00001, 0b00011, 0b00111, 0b01111, 0b11111
+			// Use the following LED pattern to indicate battery level
+			// 0b00000, 0b00001, 0b00011, 0b00111, 0b01111, 0b11111 (displayed LTR)
 			buffer[44 + isBluetooth] = (this->batteryLevel == 0)	 ? 0
 									   : (this->batteryLevel >= 100) ? 31
 																	 : (1 << ((this->batteryLevel * 5) / 100)) - 1;
